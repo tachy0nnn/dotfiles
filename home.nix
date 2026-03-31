7cf2be29e2ee5657992570fd736f56590f51e0d1
Yon <154914363+tachy0nnn@users.noreply.github.com>
2026-03-31 14:01:11 +0000

    feat: add SSH config

--- home.nix
+++ home.nix
@@ -131,6 +131,20 @@
     };
   };
 
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
+
+  services.ssh-agent.enable = true;
+
   programs.zoxide = {
     enable = true;
     enableBashIntegration = true;
